a4badd8d9d149995906e54a9d898ea16966c0f8d
Prejith Premkumar <prajithpremg@gmail.com>
2018-05-28 21:56:15 +0530

    connect redux to react

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('rnapp', () => App);
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore'
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store ={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent('rnapp', () => RNRedux);
