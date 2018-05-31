c0ddf226a3a92d74d2f4ef64601c4d2542b3c8d5
Prejith Premkumar <prajithpremg@gmail.com>
2018-05-31 08:51:52 +0530

    add content to screens

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'; 
 
 class FindPlaceScreen extends Component  {
-
+    return() {
+        return(
+            <View>
+                <Text>SharePlaceScreen</Text>
+            </View>
+        );
+    }
 }
 
 export default FindPlaceScreen;
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'; 
+import { View, Text } from 'react-native';
 
 class SharePlaceScreen extends Component  {
-
+    return() {
+        return(
+            <View>
+                <Text>SharePlaceScreen</Text>
+            </View>
+        );
+    }
 }
 
 export default SharePlaceScreen;
