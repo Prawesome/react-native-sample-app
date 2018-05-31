d06aa7f859ef43ce46172e0c0028730bb6454c0a
Prejith Premkumar <prajithpremg@gmail.com>
2018-05-31 22:24:26 +0530

    implement push navigation methods

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
 import AuthScreen from './src/screens/Auth/Auth'
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import {
   Provider
 } from 'react-redux'
@@ -16,6 +17,7 @@ const store = configureStore();
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
 Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider)
+Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen);
 
 Navigation.startSingleScreenApp({
   screen: {
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,44 +1,44 @@
-import {
-    Navigation
-} from 'react-native-navigation';
-import Icon from 'react-native-vector-icons/Ionicons'
+import { Navigation } from "react-native-navigation";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const startTabs = () => {
-        Promise.all([
-            Icon.getImageSource("md-map", 30),
-            Icon.getImageSource("md-share-alt", 30)
-        ]).then(source => {
-            Navigation.startTabBasedApp({
-                tabs: [{
-                    screen: "awesome-places.FindPlaceScreen",
-                        label: "Find Place",
-                        title: "Find Place",
-                        icon: source[0]
-                    },
-                    {
-                        screen: "awesome-places.SharePlaceScreen",
-                        label: "Share Place",
-                        title: "Share Place",
-                        icon: source[1]
-                    }
-                ]
-            });
-        });
-
-            Navigation.startTabBasedApp({
-                tabs: [{
-                        screen: "awesome-places.FindPlaceScreen",
-                        label: "Find Place",
-                        title: "Find Place",
-                        icon: Icon.getImageSource("")
-                    },
-                    {
-                        screen: "awesome-places.SharePlaceScreen",
-                        label: "Share Place",
-                        title: "Share Place"
-                    }
-                ]
-            });
+  Promise.all([
+    Icon.getImageSource("md-map", 30),
+    Icon.getImageSource("md-share-alt", 30)
+  ]).then(source => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: "awesome-places.FindPlaceScreen",
+          label: "Find Place",
+          title: "Find Place",
+          icon: source[0]
+        },
+        {
+          screen: "awesome-places.SharePlaceScreen",
+          label: "Share Place",
+          title: "Share Place",
+          icon: source[1]
         }
+      ]
+    });
+  });
 
-        export default startTabs;
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        screen: "awesome-places.FindPlaceScreen",
+        label: "Find Place",
+        title: "Find Place",
+        icon: Icon.getImageSource("")
+      },
+      {
+        screen: "awesome-places.SharePlaceScreen",
+        label: "Share Place",
+        title: "Share Place"
+      }
+    ]
+  });
+};
+
+export default startTabs;
