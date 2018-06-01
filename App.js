069ea4583c157f8f586931e00558da34b1a2d071
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-01 08:32:48 +0530

    implement nav drawer

--- App.js
+++ App.js
@@ -11,13 +11,15 @@ import {
 } from 'react-redux'
 
 import configureStore from './store/configureStore';
+import SideDrawerScreen from './src/screens/SideDrawer/SideDrawer';
 
 const store = configureStore();
 
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
 Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
-Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider)
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent("awesome-places.SideDrawerScreen", () => SideDrawerScreen);
 
 Navigation.startSingleScreenApp({
   screen: {
--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -4,7 +4,8 @@ import Icon from "react-native-vector-icons/Ionicons";
 const startTabs = () => {
   Promise.all([
     Icon.getImageSource("md-map", 30),
-    Icon.getImageSource("md-share-alt", 30)
+    Icon.getImageSource("md-share-alt", 30),
+    Icon.getImageSource("ios-menu", 30)
   ]).then(source => {
     Navigation.startTabBasedApp({
       tabs: [
@@ -12,15 +13,38 @@ const startTabs = () => {
           screen: "awesome-places.FindPlaceScreen",
           label: "Find Place",
           title: "Find Place",
-          icon: source[0]
+          icon: source[0],
+          navigatorButtons: {
+              leftButtons: [
+                {
+                    icon: source[2],
+                    title: "Menu",
+                    id: "sideDrawerToggle"
+                }
+              ]
+          }
         },
         {
           screen: "awesome-places.SharePlaceScreen",
           label: "Share Place",
           title: "Share Place",
-          icon: source[1]
+          icon: source[1],
+          navigatorButtons: {
+              leftButtons: [
+                {
+                    icon: source[2],
+                    title: "Menu",
+                    id: "sideDrawerToggle"
+                }
+              ]
+          }
         }
-      ]
+      ],
+      drawer: {
+          left: {
+              screen: "awesome-places.SideDrawerScreen"
+          }
+      }
     });
   });
 
