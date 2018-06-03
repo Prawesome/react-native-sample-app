d266fdce80185d93cebd2f4b362f2e8e2b99923a
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-03 21:37:37 +0530

    create new screens

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native";
 import { connect } from "react-redux";
 
 import PlaceInput from "../../components/PlaceInput/PlaceInput";
 import { addPlace } from "../../../store/actions/index";
+
+import MainText from "../../components/UI/MainText/MainText";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
+import PickImage from "../../components/PickImage/PickImage"
+import PickLocation from "../../components/PickLocation/PickLocation";
 
 class SharePlaceScreen extends Component {
   constructor(props) {
@@ -27,12 +39,39 @@ class SharePlaceScreen extends Component {
 
   render() {
     return (
-      <View>
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <MainText>
+            <HeadingText>Share a place with us</HeadingText>
+          </MainText>
+          <PickImage />
+          <PickLocation />
+          <PlaceInput />
+          <View style={styles.button}>
+            <Button title="Share the place" onPress={ () => alert("Share place")}/>
+          </View>
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  placeholder: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#eee",
+    width: "80%",
+    height: 300
+  },
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  button: {
+    margin: 8
+  }
+});
 
 const mapDispatchToProps = dispatch => {
   return {
