3e82ca5700626b02d77fb5fe7058c6c5923563c3
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-12 09:40:42 +0300

    map inital load config + add map to location pick screen

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Dimensions } from "react-native";
+import MapView from "react-native-maps";
 
 class PickLocation extends Component {
+  state = {
+    focusedLocation: {
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta:
+        (Dimensions.get("window").width / Dimensions.get("window").height) *
+        0.0122
+    }
+  };
+
+  pickLocationHandler = event => {
+    const coords = event.nativeEvent.coordinate;
+    this.setState( prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        }
+      }
+    })
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.placeholder}>
-          <Text>Map</Text>
-        </View>
+        <MapView
+          initialRegion={this.state.focusedLocation}
+          style={styles.map}
+          region={this.state.focusedLocation}
+          onPress={this.pickLocationHandler}
+        />
         <View style={styles.button}>
-          <Button title="Locate me" onPress={ () => alert('Locaiton picked')} />
+          <Button title="Locate me" onPress={() => alert("Locaiton picked")} />
         </View>
       </View>
     );
@@ -17,19 +45,16 @@ class PickLocation extends Component {
 }
 
 const styles = StyleSheet.create({
-  placeholder: {
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "#eee",
-    width: "80%",
-    height: 300
+  map: {
+    width: "100%",
+    height: 250
   },
   button: {
     margin: 8
   },
   container: {
-      width: "100%",
-      alignItems: "center"
+    width: "100%",
+    alignItems: "center"
   }
 });
 
