b4124ac01b990955e2425b504d4bb8be25b7b7d5
Prejith Premkumar <prajithpremg@gmail.com>
2018-06-13 08:12:21 +0300

    add firebase storage support

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+const fs = require("fs");
+const UUID = require("uuid-v4");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const gcconfig = {
+    projectId: "reactnativesampl-1528734608974", 
+    keyFilename: "reactnativesample.json"
+};
+
+const gcs = require("@google-cloud/storage")(gcconfig);
+
+exports.storeImage = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+      const body = JSON.parse(request.body);
+      fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", (err) => {
+         console.log(err);
+         return response.status(500).json({error: err});
+      });
+      const bucket = gcs.bucket("reactnativesampl-1528734608974.appspot.com");
+      const uuid = UUID();
+
+      bucket.upload("/tmp/uploaded-image.jpg", {
+          uploadType: "media",
+          destination: "/places/" + uuid + ".jpg", 
+          metadata: {
+              contentType: "image/jpeg",
+              firebaseStorageDownloadTokens: uuid
+          }
+      })
+  });
+  response.send("Hello from Firebase!");
+});
